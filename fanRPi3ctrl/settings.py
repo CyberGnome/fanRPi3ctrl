bddd788708d1bddb0d9803064f1e9ee744a2c5d1
cyber gnome <cybergnome.DEADC0DE@gmail.com>
2018-08-30 01:45:32 +0300

    change settings for a release

--- fanRPi3ctrl/settings.py
+++ fanRPi3ctrl/settings.py
@@ -4,12 +4,12 @@ pin = 18         # control pin
 
 
 # Fan settings
-temp_on = 40.0   # fan start temperature
+temp_on = 43.0   # fan start temperature
 delay_ms = 5000  # interval between CPU temperature checks
 
 
 # Debug Mode
-debug = 3       # input number [0..3] where:
+debug = 0       # input number [0..3] where:
                 #     0 - disable debug messages
                 #     1 - enable debug messages as start/stop fan
                 #     2 - print CPU temperature
