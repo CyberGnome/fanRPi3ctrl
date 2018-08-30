91254374418ba9465288b7dd3dc518ac559faf0d
cyber gnome <cybergnome.DEADC0DE@gmail.com>
2018-08-31 00:52:19 +0300

    add temperature for off fan

--- fanRPi3ctrl/fanRPi3ctrl.py
+++ fanRPi3ctrl/fanRPi3ctrl.py
@@ -5,62 +5,88 @@ import RPi.GPIO as GPIO
 
 import settings as params
 
-
-def print_log(msg, lvl=3):
-    if lvl < 1:
-        return
-
-    if lvl <= params.debug:
-        print("LOG: %s [%s]" % (msg, str(datetime.datetime.now())))
-
-
-def cleanup():
-    GPIO.cleanup()
-    print_log("GPIO cleanup")
+# В следующую версию (v0.2) добавить:
+# 1. температуру отключения куллера
+# 2. логирование в файл
+# 3. сделать возможным увеличить время работы куллера
+#    за счет увеличения в n раз (по умолчанию в 3) времени
+#    проверки температуры, когда куллер уже работает
+# 4. Переписать всё в стиле ООП
+# 5. Созадать инсталляционный скрипт (можно отдельной версией v0.2.1)
 
 
-def setup():
-    GPIO.cleanup()
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(params.pin, GPIO.OUT)
-    GPIO.setwarnings(False)
-    print_log("GPIO setup")
+class Logs:
+    def __init__(self, debug_lvl):
+        self.dbg_lvl = debug_lvl
+
+    def out(self, msg, lvl=3):
+        if self.dbg_lvl == 0 or lvl == 0:
+            return
+
+        if lvl <= self.dbg_lvl:
+            print("LOG: %s [%s]" % (msg, str(datetime.datetime.now())))
 
 
-def get_cpu_temp():
-    res = os.popen("vcgencmd measure_temp").readline()
-    temp = (res.replace("temp=", "").replace("'C\n", ""))
-    print_log("CPU temperature is %s" % temp, 2)
-    return float(temp)
+class SystemControl:
+    def __init__(self):
+        self.h_logs = Logs(params.debug)
+
+    def get_cpu_temp(self):
+        res = os.popen("vcgencmd measure_temp").readline()
+        temp = (res.replace("temp=", "").replace("'C\n", ""))
+        self.h_logs.out("CPU temperature is %s" % temp, 2)
+        return float(temp)
+
+    def clean_gpio(self):
+        GPIO.cleanup()
+        self.h_logs.out("GPIO cleanup")
+
+    def setup_gpio(self):
+        self.clean_gpio()
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(params.pin, GPIO.OUT)
+        GPIO.setwarnings(False)
+
+        self.h_logs.out("GPIO setup")
+
+    def set_pin(self, mode):
+        GPIO.output(params.pin, mode)
+        if mode is True:
+            self.h_logs.out("Fan ON", 1)
+        else:
+            self.h_logs.out("Fan OFF", 1)
 
 
-def set_pin(mode):
-    GPIO.output(params.pin, mode)
-    if mode is True:
-        print_log("Fan ON", 1)
-    else:
-        print_log("Fan OFF", 1)
+class FanRPi3ctrl:
+    def __init__(self, t_on, t_off, delay_ms):
+        self.fan_on = False
+        self.temp_on = t_on
+        self.temp_off = t_off
+        self.delay_time = delay_ms
+        self.sysctl = SystemControl()
+        self.time_factor = 3.0
 
+    def fan_on(self):
+        self.sysctl.set_pin(True)
 
-def fan_on():
-    set_pin(True)
+    def fan_off(self):
+        self.sysctl.set_pin(False)
 
+    def delay(self):
+        s = float(self.delay_time) / 1000.0
+        self.sysctl.set_pin("Wait %f seconds..." % s)
+        sleep(s)
 
-def fan_off():
-    set_pin(False)
-
-
-def control_temp():
-    cpu_temp = get_cpu_temp()
-    if cpu_temp >= params.temp_on:
-        fan_on()
-    else:
-        fan_off()
-
-
-def delay(ms):
-    s = float(ms) / 1000.0
-    print_log("Wait %f seconds..." % s)
-    sleep(s)
-
-
+    def fan_control(self):
+        cpu_temp = self.sysctl.get_cpu_temp()
+        if self.fan_on is False:
+            if cpu_temp >= self.temp_on:
+                self.fan_on()
+                self.fan_on = True
+                self.delay_time * self.time_factor
+        else:
+            if cpu_temp <= self.temp_off:
+                self.fan_off()
+                self.fan_on = False
+                self.delay_time / self.time_factor
--- fanRPi3ctrl/settings.py
+++ fanRPi3ctrl/settings.py
@@ -4,13 +4,20 @@ pin = 18         # control pin
 
 
 # Fan settings
-temp_on = 45.0   # fan start temperature
-delay_ms = 5000  # interval between CPU temperature checks
+temp_on = 48.5   # fan start temperature
+temp_off = 40.0  # fan stop temperature
+
+delay_ms = 15000  # interval between CPU temperature checks
 
 
 # Debug Mode
-debug = 0       # input number [0..3] where:
+debug = 3       # input number [0..3] where:
                 #     0 - disable debug messages
                 #     1 - enable debug messages as start/stop fan
                 #     2 - print CPU temperature
                 #     3 - print all messages
+
+output_file = False  # if it is True logs output to file
+logs_file = "/home/.fanRPi3ctrl.logs"  # path to logs file
+                                       # if "output_file" is False
+                                       # this parameter is ignored
