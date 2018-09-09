7164f36fdfd2e9fb98ae22019124499bcf6c8888
cyber gnome <cybergnome.DEADC0DE@gmail.com>
2018-09-09 12:39:29 +0300

    change run file: add optional file output

--- fanRPi3ctrl/run.py
+++ fanRPi3ctrl/run.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+import outredirect
 import fanRPi3ctrl
 import settings as params
 
 
 if __name__ == "__main__":
+    print_redirect = outredirect.OutRedirect()
+    if params.output_file is True:
+        try:
+            print_redirect.file(params.logs_file)
+        except Exception as error:
+            raise
+
     fan_ctrl = fanRPi3ctrl.FanRPi3ctrl(params.temp_on,
                                        params.temp_off,
                                        params.delay_ms)
@@ -15,3 +23,5 @@ if __name__ == "__main__":
             fan_ctrl.delay()
     except KeyboardInterrupt:
         fan_ctrl.sysctl.clean_gpio()
+        if params.output_file is True:
+            print_redirect.standard()
